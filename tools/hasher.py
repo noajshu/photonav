443d76e11028eea626622a36bc9dc55869385b37
Noah Shutty <noahshutty@gmail.com>
2017-01-08 01:17:43 -0800

    added templates

---
+++ tools/hasher.py
@@ -0,0 +1,15 @@
+import hashlib
+
+
+BUF_SIZE = 65536
+
+def md5(fpath):
+    md5 = hashlib.md5()
+    with open(fpath, 'rb') as infile:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            md5.update(data)
+
+    return md5.hexdigest()
